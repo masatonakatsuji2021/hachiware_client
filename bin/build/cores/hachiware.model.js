2d3af70672dcb08581429707629e16affa76e00e
nakajima-satoru <nakajima@email.com>
2022-03-03 11:44:18 +0900

    3/3

--- bin/build/cores/hachiware.model.js
+++ bin/build/cores/hachiware.model.js
@@ -1,4 +1,11 @@
 hachiware.loadModel = function(modelName, options){
 
-	return new hachiware.loadCore("models",modelName,options,[]);
+	return new hachiware.loadCore("models",modelName,options,["before"], function(){
+
+		if(this.$base.before){
+			this.$base.before.bind(this)();
+		}
+
+	});
+
 };
